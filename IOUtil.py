a21c666799df782970637a5cc1bc6d8712497673
yuhan.l.li <yuhan.l.li@Oracle.com>
2017-09-06 18:31:52 +0800

    cal bukect result

--- IOUtil.py
+++ IOUtil.py
@@ -1,5 +1,12 @@
 from tfidfOper import *
 
+MATRIX_FILE = "/Users/yuhanli/PycharmProjects/TFIDFImple/out/matrix.out"
+OUTPUT_FILE = "/Users/yuhanli/PycharmProjects/TFIDFImple/out/tfidf.out"
+SORTED_MATRIXSUM_FILE = "/Users/yuhanli/PycharmProjects/TFIDFImple/out/sortedMatSum.out"
+
+# MATRIX_FILE = "H:\VSProject\TFIDFimple\logs\matrix.out"
+# OUTPUT_FILE = "H:\\VSProject\\TFIDFimple\\logs\\tfidf.out"
+
 class IOUtil(object):
     "Do read file by lines"
     def __init__(self, fileName):
@@ -32,14 +39,30 @@ class IOUtil(object):
                 k = ' '.join([str(j) for j in i])
                 file.write(k + "\n")
             file.close()
-
+    def getSortedMatSum(self, weightArray):
+        weightSumList = weightArray.sum(axis=1).tolist()
+        templateWeightMap = {}
+        for i, tmp in enumerate(self.tempList):
+            templateWeightMap[tmp] = weightSumList[i][0]
+        # print templateWeightMap
+        return sorted(templateWeightMap.items(), lambda x, y: cmp(x[1], y[1]), reverse=True)
+    def getBucketList(self, weightArray):
+        sumMat = self.getSortedMatSum(weightArray)
+        BucketMap = {}
+        for key, value in sumMat:
+            if not value in BucketMap:
+                BucketMap[value] = [key]
+            else:
+                BucketMap[value].append(key)
+        #ret = []
+        #for key in sorted(BucketMap.keys()):
+        #    ret.append((key, BucketMap[key]))
+        ret = [(key, BucketMap[key]) for key in sorted(BucketMap.keys(), reverse = True)]
+        return ret
 
 if __name__ == "__main__":
-    #MATRIX_FILE = "H:\VSProject\TFIDFimple\logs\matrix.out"
-    # OUTPUT_FILE = "H:\\VSProject\\TFIDFimple\\logs\\tfidf.out"
-    MATRIX_FILE = "/Users/yuhanli/PycharmProjects/TFIDFImple/out/matrix.out"
-    OUTPUT_FILE = "/Users/yuhanli/PycharmProjects/TFIDFImple/out/tfidf.out"
-    SORTED_MATRIXSUM_FILE = "/Users/yuhanli/PycharmProjects/TFIDFImple/out/sortedMatSum.out"
+
+
 
 
     ioutil = IOUtil(MATRIX_FILE)
@@ -47,19 +70,16 @@ if __name__ == "__main__":
     appList = ioutil.convertToList(readList)
     tfidfIns = tfidfOper()
     weightArray = tfidfIns.transformer(appList)
-    weightSumList = weightArray.sum(axis=1).tolist()
+
 
     # print weightArray
-    print (weightArray.sum(axis=1).tolist())
-    print (ioutil.getTmpList())
-
-    templateWeightMap = {}
-    for i, tmp in enumerate(ioutil.getTmpList()):
-        templateWeightMap[tmp] = weightSumList[i][0]
-    print templateWeightMap
-
-    a = sorted(templateWeightMap.items(), lambda x, y: cmp(x[1], y[1]), reverse=True)
-
-    ioutil.writeMatrix(a, SORTED_MATRIXSUM_FILE)
+    # print (weightArray.sum(axis=1).tolist())
+    # print (ioutil.getTmpList())
+    sortedMatSumList = ioutil.getSortedMatSum(weightArray)
+    result = ioutil.getBucketList(weightArray)
+    for x in result:
+        print x
+    print len(result)
+    # ioutil.writeMatrix(sortedMatSumList, SORTED_MATRIXSUM_FILE)
     # print type(weightArray)
     # ioutil.writeMatrix(weightArray, OUTPUT_FILE)
